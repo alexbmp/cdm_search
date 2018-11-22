4283f9ca1b5c6a48ceec771aa9a6b5d131a89c4a
Seongmin Choi <alex2bmp@gmail.com>
2018-11-22 09:24:15 +0900

    applied pagination

--- meas_lab/views.py
+++ meas_lab/views.py
@@ -14,40 +14,23 @@ def search_form(request):
 def search(request):
     template = "meas_lab/search.html"
     query = request.GET.get('q')
+    page = request.GET.get('page')
+    if not page:
+        page = 1
     if query:
         codes = Code.objects.filter(Q(division__name="Measurement lab")
             & (Q(source_id__icontains=query)
             | Q(source_name__icontains=query)
             | Q(target_name__icontains=query)))
         if codes:
-            context = {'codes': codes}
+            paginator = Paginator(codes, 10)
+            codes = paginator.get_page(page)
+            context = {'codes': codes, 'q': query}
             return render(request, template, context)
         else:
-            return HttpResponse('No results found.')
+            context = {'message': 'No results found.'}
+            return render(request, template, context)
     else:
-        return HttpResponse("You should at least search for something.")
+        context = {'message': 'You should at least search for something.'}
+        return render(request, template, context)
 
-    #paginator = Paginator(object_list, 10)
-    #page = request.GET.get('page')
-    #try:
-    #    items = paginator.page(page)
-    #except PageNotAnInteger:
-    #    items = paginator.page(1)
-    #except EmptyPage:
-    #    items = paginator.page(paginator.num_pages)
-
-    #context = {
-    #    'items': items,
-    #}
-    """
-    if 'q' in request.GET and request.GET['q']:
-        q = request.GET['q']
-        codes = Code.object.filter(division__name="Measurement lab")
-    else:
-        return HttpResponse("Please submit a search term.")
-
-    # division <- Division instance that has name as attribute
-    template = 'meas_lab/search.html'
-    context = {'codes': codes}
-    return render(request, template, context)
-"""
